752d6cca3e058d76970e5ba63a2f4670a4129533
helloredreyes <hello.silentlymine@gmail.com>
2025-12-10 05:55:28 +0700

    Added About and Footer, Removed Settings, Gift Ideas and Notifications

--- ConnectionsManager/Data/Note.cs
+++ ConnectionsManager/Data/Note.cs
@@ -3,6 +3,7 @@ namespace ConnectionsManager.Data
     public class Note
     {
         public int Id { get; set; } // primary key
+        public string? UserId { get; set; } 
         public string Topic { get; set; } = string.Empty;
         public string Details { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
